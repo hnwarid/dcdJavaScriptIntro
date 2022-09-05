f0d6253cb338e27d3cc7a9c7f5b60b32628be763
Hamzah Nurwarid <hn.warid@gmail.com>
2022-09-05 15:11:46 +0700

    initial commit through VS Code

---
+++ 0 0 Variable dan Tipe Data .js
@@ -0,0 +1,22 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = "Hamzah";
+let lastName = "Nurwarid";
+let age = 17;
+let isMarried = false; /*yet...*/
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dcdJavaScriptIntro
+<br>
+This repo contains codes I wrote for Kuis Coding submissions on 
+<a href="https://www.dicoding.com/academies/256">
+Belajar Dasar Pemograman JavaScript on Dicoding  </a> 
+<br>
+<a href="https://www.dicoding.com/certificates/ERZRM8DQMPYV">
+The certificate obtained after I finished the course </a>
